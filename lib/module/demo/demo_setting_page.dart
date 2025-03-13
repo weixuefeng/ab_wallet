0d68940016e2675826d81008da364a3942d9bc9b
ahan <ahan0530@163.com>
2025-03-13 15:30:35 +0800

    fix：run abwallet success

--- lib/module/demo/demo_setting_page.dart
+++ lib/module/demo/demo_setting_page.dart
@@ -1,28 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_trust_wallet_core/flutter_trust_wallet_core.dart';
-import 'package:flutter_trust_wallet_core/trust_wallet_core_ffi.dart';
 import 'package:force_wallet/generated/l10n.dart';
 import 'package:force_wallet/providers/locale_provider.dart';
 import 'package:force_wallet/providers/theme_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:lib_base/lib_base.dart';
-import 'package:lib_chain_manager/mock/mock_ab_chain_manager_impl.dart';
-import 'package:lib_storage/lib_storage.dart';
-import 'package:lib_wallet_manager/impl/ab_wallet_manager.dart';
-import 'package:lib_web3_core/impl/chain_method_impl.dart';
-import 'package:lib_web3_core/lib_web3_core.dart';
-import 'package:lib_web3_core/utils/wallet_method_extension.dart';
-import 'package:lib_web3_core/utils/wallet_method_util.dart';
-import 'package:lib_web3_interaction/chains/rpc/rpc_address_provider.dart';
-import 'package:lib_web3_interaction/mock/mock_lib_ab_web3_core_module_impl.dart';
 
 class DemoSettingPage extends HookConsumerWidget {
   const DemoSettingPage({super.key});
 
   static const String TAG = 'DemoSettingPage';
-
-
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
